7d471934214f501f9e26ced762e03b59eba3e9f9
AnikaTahashin10 <anikatahashin90@gmail.com>
2023-10-08 23:35:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5427907e7f5beab0c95321cea472cc97`
+
+  const searchLocation = (event) =>{
+    if(event.key === 'Enter'){
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+}
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+        value = {location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type = "text"/>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <p>60° F</p>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>65° F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+            <div className="wind">
+              <p className="bold">12 MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
